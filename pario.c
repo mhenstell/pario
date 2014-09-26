68351a9848a3d0a5c55e680cf302ec7c7b37f789
Max Henstell <git@kapamaki.net>
2014-09-26 16:16:23 -0400

    updated gpios in pario.c, added test dts

--- pario.c
+++ pario.c
@@ -24,19 +24,19 @@
  * \todo: Find a way to unify this with the defines in the .p file
  */
 static const uint8_t gpios0[] = {
-	23, 27, 22, 10, 9, 8, 26, 11, 30, 31, 5, 3, 20, 4, 2, 14, 7, 15
+	
 };
 
 static const uint8_t gpios1[] = {
-	13, 15, 12, 14, 29, 16, 17, 28, 18, 19,
+	12, 13
 };
 
 static const uint8_t gpios2[] = {
-	2, 5, 22, 23, 14, 12, 10, 8, 6, 3, 4, 1, 24, 25, 17, 16, 15, 13, 11, 9, 7,
+	6, 7, 8, 9, 10, 11, 12, 13, 22, 23, 24
 };
 
 static const uint8_t gpios3[] = {
-	21, 19, 15, 14, 17, 16
+	14, 15, 16, 17, 19, 21
 };
 
 #define ARRAY_COUNT(a) ((sizeof(a) / sizeof(*a)))
